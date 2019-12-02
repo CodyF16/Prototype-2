0bddf7477eb5a6797c910cd04286c5218ae982a8
CodyF15 <56046626+CodyF16@users.noreply.github.com>
2019-12-02 11:58:02 -0800

    player positioning

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizantalInput;
+    public float speed = 10.0f;
+    public float leftBoundary = -15.0f;
+    public float xRange = 15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+         }
+         if (transform.position.x > xRange)
+         {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+         }
+        horizantalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizantalInput * Time.deltaTime * speed);
+    }
+}
