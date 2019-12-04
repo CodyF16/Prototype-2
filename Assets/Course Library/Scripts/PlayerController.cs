92ff44f17a960c3a79e6acb499551092782da536
CodyF15 <56046626+CodyF16@users.noreply.github.com>
2019-12-04 11:20:35 -0800

    food fight

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizantalInput;
+    public float speed = 10.0f;
+    public float leftBoundary = -15.0f;
+    public float xRange = 15.0f;
+    public GameObject projectilePrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Boundarys
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+         }
+
+         if (transform.position.x > xRange)
+         {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+         }
+
+        //Player movement
+        horizantalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizantalInput * Time.deltaTime * speed);
+
+        //Projectile
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
